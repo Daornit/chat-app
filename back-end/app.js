ac24f1c81e98befbc49993962393552bb3f13669
Daornit <daornit2@gmail.com>
2020-03-14 13:01:18 +0800

    Auth using jwt

--- back-end/app.js
+++ back-end/app.js
@@ -1,41 +1,77 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var errorHandler = require('errorhandler');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
 
 var app = express();
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'hehe you will never find out', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+if(!isProduction) {
+  app.use(errorHandler());
+}
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// configure mongoose
+mongoose.connect('mongodb://localhost/chat-app-test');
+mongoose.set('debug', true);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// models
+require('./models/Users');
 
-  // render the error page
+// password config must be below all models
+require('./config/passport');
+
+app.use(require('./routes'))
+
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res) => {
+    console.log("res :: ", res);
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
   res.status(err.status || 500);
-  res.render('error');
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
 });
 
 module.exports = app;
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('test');
   res.render('index', { title: 'Express' });
 });
+
+router.use('/api', require('./api'));
 
 module.exports = router;
